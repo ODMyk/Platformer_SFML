abdeaa09770eec2f55a7568d222d0f0b3c0724a9
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2023-03-11 23:28:00 +0200

    Created template for more comfortable commands usage

--- Headers/Command.hpp
+++ Headers/Command.hpp
@@ -15,6 +15,12 @@ struct Command {
 	unsigned int category;
 };
 
-
+template <typename GameObject, typename Function>
+std::function<void(SceneNode&, sf::Time)> derivedAction(Function fn) {
+	return [=] (SceneNode& node, sf::Time dt) {
+		assert(dynamic_cast<GameObject*>(&node) != nullptr);
+		fn(static_cast<GameObject&>(node), dt);
+	};
+}
 
 #endif
