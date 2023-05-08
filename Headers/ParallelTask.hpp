5eb4220d5ca48a2662d77e966ee164829b4b6f74
ODMyk <ostapenko.dmytro05@gmail.com>
2023-05-08 21:53:05 +0300

    Declared ParallelTask class

---
+++ Headers/ParallelTask.hpp
@@ -0,0 +1,26 @@
+#ifndef PARALLELTASK_HPP
+#define PARALLELTASK_HPP
+
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Lock.hpp>
+#include <SFML/System/Mutex.hpp>
+#include <SFML/System/Thread.hpp>
+
+class ParallelTask {
+public:
+  ParallelTask();
+  void execute();
+  bool isFinished();
+  float getCompletion();
+
+private:
+  void runTask();
+
+private:
+  sf::Thread mThread;
+  bool mFinished;
+  sf::Clock mElapsedTime;
+  sf::Mutex mMutex;
+};
+
+#endif
