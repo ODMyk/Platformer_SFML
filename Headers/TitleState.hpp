8b0c28ec39f4d13678532cedfa0da2155a3104af
ODMyk <ostapenko.dmytro05@gmail.com>
2023-05-08 20:50:22 +0300

    Declared TitleState class

---
+++ Headers/TitleState.hpp
@@ -0,0 +1,21 @@
+#ifndef TITLESTATE_HPP
+#define TITLESTATE_HPP
+
+#include "SFML/Graphics.hpp"
+#include "State.hpp"
+
+class TitleState : public State {
+public:
+  TitleState(StateStack &stack, Context context);
+  virtual void draw();
+  virtual bool update(sf::Time dt);
+  virtual bool handleEvent(const sf::Event &event);
+
+private:
+  sf::Sprite mBackgroundSprite;
+  sf::Text mText;
+  bool mShowText;
+  sf::Time mTextEffectTime;
+};
+
+#endif
