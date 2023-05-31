e097ae7c1da9a82bc07c0259e2275042c850e256
ODMyk <ostapenko.dmytro05@gmail.com>
2023-05-31 13:04:53 +0300

    push all changes

--- Headers/World1.hpp
+++ Headers/World1.hpp
@@ -2,16 +2,19 @@
 #define WORLD1_HPP
 
 #include "World.hpp"
+#include "Barrier.hpp"
 
 class World1: public World {
 public:
   explicit World1(sf::RenderWindow &window);
   void update(sf::Time timedelta) override;
-  void draw() override;
+  //void draw() override;
 
 protected:
   void loadTextures() override;
   void buildScene() override;
+  void processCollisions();
+  std::vector<Barrier> mBarriers;
 };
 
 #endif
--- World1.cpp
+++ World1.cpp
@@ -7,17 +7,33 @@ World1::World1(sf::RenderWindow& window): World(window) {
   buildScene();
 }
 
-void World1::update(sf::Time delta) { World::update(delta); }
+void World1::processCollisions() {
+  sf::Vector2f vel = mPlayerAvatar->getVelocity();
 
-void World1::draw() { World::draw(); }
+}
+
+void World1::update(sf::Time timedelta) {
+  mPlayerAvatar->setVelocity(0.f, 0.f);
+
+  while (!mCommandQueue.isEmpty()) {
+    mSceneGraph.onCommand(mCommandQueue.pop(), timedelta);
+  }
+
+  processCollisions();
+
+  mSceneGraph.update(timedelta);
+
+  sf::Vector2f position = mPlayerAvatar->getPosition();
+  mWorldView.setCenter(position);
+}
 
 void World1::loadTextures() {
   mTextures.load(Textures::Background, "resources/img/Background.png");
-  mTextures.load(Textures::Avatar, "resources/img/Player.jpg");
+  mTextures.load(Textures::Avatar, "resources/img/Player.png");
 }
 
 void World1::buildScene() {
-  std::unique_ptr<Avatar> leader(new Avatar(Avatar::standard, mTextures));
+  std::unique_ptr<Avatar> leader(new Avatar(Avatar::standard, mTextures, {64.f, 64.f}));
   mPlayerAvatar = leader.get();
   mPlayerAvatar->setPosition(mSpawnPosition);
   mSceneLayers[Foreground]->attachChild(std::move(leader));
